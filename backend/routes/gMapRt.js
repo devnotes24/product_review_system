751723465490e8198978fb46c22af26a90e7e589
Sachinvb <sachinvb27@gmail.com>
2024-09-16 12:39:08 +0530

    first commit

--- backend/routes/gMapRt.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const gMapCt = require('../Controller/gMapCt');
-
-router.post('/nearby', gMapCt.gMap);
-module.exports = router;
