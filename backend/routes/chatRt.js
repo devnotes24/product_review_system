0aef0611e35a4ef3e60f771706e8ddb1adc0302e
Sachinvb <sachinvb27@gmail.com>
2024-09-17 12:54:23 +0530

    updated the developers info

--- backend/routes/chatRt.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-const chatCt = require('../Controller/chatCt');
-
-router.post('/chat', chatCt.chatFn);
-
-module.exports = router;
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -4,5 +4,4 @@ const router = express.Router();
 
 router.use('/regLoginRt', require('./regLoginRt'));
 router.use('/productDataRt', require('./productDataRt'));
-router.use('/chatRt', require('./chatRt'));
 module.exports = router;
